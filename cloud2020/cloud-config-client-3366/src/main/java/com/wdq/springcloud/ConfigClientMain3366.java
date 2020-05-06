d078efc378521576df754fbb62b6df3d06298ee3
王栋卿 <bestriven666@outlook.com>
2020-05-06 18:35:58 +0800

    springcloud bus结合RabbitMQ测试完成

---
+++ cloud2020/cloud-config-client-3366/src/main/java/com/wdq/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,18 @@
+package com.wdq.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Auther: wdq
+ * @Date: 2020/5/6 17:55
+ * @Description:
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class,args);
+    }
+}
