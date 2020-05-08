bae845eb9a44faf70b901b3402db661f32a4c9ad
王栋卿 <bestriven666@outlook.com>
2020-05-08 13:18:25 +0800

    alibaba nacos服务注册于发现、中心配置、集群配置测试完成

---
+++ cloud2020/cloudalibaba-consumer-nacos-order84/src/main/java/com/wdq/springcloud/OrderNacosMain84.java
@@ -0,0 +1,18 @@
+package com.wdq.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Auther: wdq
+ * @Date: 2020/5/7 18:19
+ * @Description:
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderNacosMain84 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderNacosMain84.class,args);
+    }
+}
---
+++ cloud2020/cloudalibaba-provider-payment9002/src/main/java/com/wdq/springcloud/PaymentMain9002.java
@@ -0,0 +1,18 @@
+package com.wdq.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Auther: wdq
+ * @Date: 2020/5/7 16:17
+ * @Description:
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9002 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9002.class,args);
+    }
+}
